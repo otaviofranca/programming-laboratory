72af29d1c324f67e2300947e1e6b1635aa0d09ae
otavio franca <otaviofrancaofficial@gmail.com>
2024-04-18 21:05:33 -0300

    adicionado problema 03 - relatorio - validacao/entrada problema 01

--- 03.py
+++ 03.py
@@ -1,14 +1,15 @@
 '''
-1. Escreva um programa que solicita ao usuário para digitar um número inteiro e, em
-seguida, exibe esse número, o dobro e o triplo do valor desse número na tela.
+3. Escreva um programa que solicita ao usuário para digitar um número real e, em 
+seguida, exiba esse número, o dobro e o triplo do valor desse número na tela.
 '''
 def valid_number(number):
     while 1:
-        if number.isdigit():
+        try:
+            number = float(number)
             break
-        else:
-            number = input('invalid character, please type again!')    
-    return int(number)
+        except ValueError:
+            number = input('Invalid input, please enter a numeric value: ')
+    return number
 
 def request_number():
     number = input('Enter a number:')
